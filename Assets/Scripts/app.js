beaea73f2f16bbeb2df803b1ef7d94944c599f5f
Shreenath Tewari <stewari.sps@gmail.com>
2016-12-04 15:37:31 +0530

    Basic Layout

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by shreenath on 2/12/16.
- */
+$('.friend-request-icon').click(function() {
+    $(this).removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.messages-icon').click(function() {
+    $(this).removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.notifications-icon').click(function() {
+    $(this).removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.lock-icon').click(function() {
+    $(this).removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.dropdown-icon').click(function() {
+    $(this).removeClass('active');
+    $(this).addClass('active');
+});
